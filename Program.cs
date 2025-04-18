92b3f6129c5ea488e7de8ea782a188dc44ab1d45
Taylo <Taylo@MARIEL>
2025-04-17 22:33:34 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    
+       
+    {
+      
+        string myFile = new  ("MyDetails.txt");
+
+
+       using StreamWriter writer = new StreamWriter(myFile);
+        writer.WriteLine("Name: Taylor Puente");
+        writer.WriteLine("Age: 26");
+        writer.WriteLine("Name: El Paso,TX");
+        writer.Close();
+
+        StreamReader reader = new StreamReader(myFile);
+        string line;
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+
+
+
+    }
+}
